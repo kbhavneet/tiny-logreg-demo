1867fce07bed08ba24132d8a1003aaddcc02c09e
kbhavneet <kbhavneet846@gmail.com>
2025-05-19 13:09:09 +0000

    logistic regression trained incrementally

--- train_simple_model.py
+++ train_simple_model.py
@@ -13,3 +13,14 @@ class1 = np.random.randn(50, 2) + np.array([5, 5])
 X = np.vstack([class0, class1])             
 y = np.array([0]*50 + [1]*50)               
 
+# shuffle so classes are mixed
+perm = np.random.permutation(len(y)) 
+X, y = X[perm], y[perm]
+
+model = SGDClassifier(loss="log", max_iter=1, learning_rate="constant", eta0=0.1, random_state=0, tol=None, warm_start=True)
+
+accuracies = []
+for epoch in range(10):
+    model.partial_fit(X, y, classes=np.array([0, 1]))
+    acc = model.score(X, y)
+    accuracies.append(acc)
